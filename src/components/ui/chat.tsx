584cde8cc3599f35eaedbad8b6d0c9df377e0823
Bhawesh Kumar Verma <bhaweshverma50@gmail.com>
2024-02-28 13:40:53 +0530

    feat: added copy option for the response and fixed blank response

--- src/components/ui/chat.tsx
+++ src/components/ui/chat.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import { Input } from '@/components/ui/input';
 import { Button } from './button';
@@ -23,6 +23,7 @@ const ChatSection = ({
   loading: boolean;
   setLoading: any;
 }) => {
+  const [copiedMessage, setCopiedMessage] = useState<number | null>(null);
   return (
     <div className="px-8 flex flex-col gap-1 pt-10">
       <div className="flex flex-col h-full gap-4 overflow-scroll no-scrollbar">
@@ -41,33 +42,54 @@ const ChatSection = ({
                 )}
               </div>
             </div>
-            <p className={message.isBot ? 'chat-bubble flex flex-col gap-2' : 'chat-bubble chat-bubble-info'}>
-              {message.text}
-              {message.isBot && message.metadata ? (
-                <div className="pb-1">
-                  <TooltipProvider>
-                    <Tooltip>
-                      <TooltipTrigger className="text-xs text-slate-400 bg-slate-600 px-2 py-1 rounded-full">
-                        metadata
-                      </TooltipTrigger>
-                      <TooltipContent side="bottom">
-                        <p>Doc ID: {message.metadata.uid}</p>
-                        <p>Title: {message.metadata.title}</p>
-                        <p>Reference: {message.metadata.reference_text}</p>
-                        <p>Author: {message.metadata.author.uid}</p>
-                        <p>Creation Date: {new Date(message.metadata.creation_date).toLocaleString()}</p>
-                        <p>
-                          Last Updated: {new Date(message.metadata.lastChangeDate).toLocaleString()} by{' '}
-                          {message.metadata.lastChangeUser.uid}
-                        </p>
-                        {/* <p>Last Change User: {message.metadata.lastChangeUser.uid}</p>
-                        <p>Last Change Date: {new Date(message.metadata.lastChangeDate).toLocaleString()}</p> */}
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </div>
-              ) : null}
-            </p>
+            <div>
+              <p className={message.isBot ? 'chat-bubble flex flex-col gap-2' : 'chat-bubble chat-bubble-info'}>
+                {message.text && message.text.length > 0
+                  ? message.text
+                  : 'Sorry, I cannot find the answer you are looking for.'}
+                {message.isBot && message.text.length > 0 && message.metadata ? (
+                  <div className="pb-1 flex gap-2">
+                    <TooltipProvider>
+                      <Tooltip>
+                        <TooltipTrigger className="text-xs text-slate-400 bg-slate-600 px-2 py-1 rounded-full">
+                          metadata
+                        </TooltipTrigger>
+                        <TooltipContent side="bottom">
+                          <p>Doc ID: {message.metadata.uid}</p>
+                          <p>Title: {message.metadata.title}</p>
+                          <p>Reference: {message.metadata.reference_text}</p>
+                          <p>Author: {message.metadata.author.uid}</p>
+                          <p>Creation Date: {new Date(message.metadata.creation_date).toLocaleString()}</p>
+                          <p>
+                            Last Updated: {new Date(message.metadata.lastChangeDate).toLocaleString()} by{' '}
+                            {message.metadata.lastChangeUser.uid}
+                          </p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                    {message.isBot && message.text.length > 0 && (
+                      <div
+                        className={`text-xs text-slate-400 ${
+                          copiedMessage === i
+                            ? 'bg-green-700 text-white hover:bg-green-800'
+                            : 'bg-slate-600 hover:bg-slate-700'
+                        } px-2 py-1 rounded-full hover:cursor-pointer`}
+                        onClick={() => {
+                          navigator.clipboard.writeText(message.text).then(() => {
+                            setCopiedMessage(i); // Set the index of the copied message
+                            setTimeout(() => {
+                              setCopiedMessage(null); // Reset after a delay if needed
+                            }, 10000); // Reset copied status after 2 seconds
+                          });
+                        }}
+                      >
+                        {copiedMessage === i ? 'Copied' : 'Copy'}
+                      </div>
+                    )}
+                  </div>
+                ) : null}
+              </p>
+            </div>
           </div>
         ))}
         <div ref={msgEnd} />
